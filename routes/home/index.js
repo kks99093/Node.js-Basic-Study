59c3b778cd522978747088d83d9bc2669fe1ec25
kks99093 <ksk99039@naver.com>
2021-11-22 17:19:37 +0900

    git init

---
+++ routes/home/index.js
@@ -0,0 +1,15 @@
+"use strict"
+//strict모드를 사용한다는 뜻 (javascript 파일을 만들때 상단에다가 항상 적어두는게 좋다고함)
+//사용하는 이유 - 올바르지 않은 문법을 사전에 검출 할수 있다.
+// strict모드는 존재하지 않는 변수, 존재하지 않는 객체등에 대해 에러를 발생시킨다.
+
+const express = require("express");
+const router  = express.Router();
+
+const ctrl = require("./home.ctrl");
+
+
+router.get("/", ctrl.home);
+router.get("/login",ctrl.login);
+
+module.exports = router;
